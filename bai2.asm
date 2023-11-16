9155f261812a0cc2de8dae3e2fc379e69fbfa4d8
HoangAnhTuan <89626694+darkwin259@users.noreply.github.com>
2023-11-16 21:39:42 +0700

    bai2

---
+++ bai2.asm
@@ -0,0 +1,28 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+
+mov al,03h
+mov ah,0h
+mov al,1
+mov bh,0
+mov bl,11100100b
+mov cx,11h
+mov dl,30
+mov dh,12
+lea bp,str
+mov ah,13h
+int 10h
+
+str db 'cau truc may tinh'
+  
+  mov ah,4ch
+  int 21h
+ret
+
+
+
+
